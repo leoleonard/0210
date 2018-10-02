9e95d63f30a48c793037e7fc8152f7d0afa16a83
Damian Juzyk <damian.juzyk@gmail.com>
2018-10-02 19:12:44 +0200

    add schemat porownania equals

--- src/two/Product.java
+++ src/two/Product.java
@@ -24,4 +24,32 @@ public class Product {
     public void setPrice(double price) {
         this.price = price;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) { //referencja na ten sam obiekt; this to produkt 1, obj to produkt 2
+            return true;
+        }
+        if (obj == null) { //obiekt nie moze byc pusty (null)
+            return false;
+        }
+        if (!(obj instanceof Product)) { //zabezpieczenie przed porownaniem obiektow roznych typow
+            return false;
+        }
+        Product other = (Product)obj; // rzutowanie by odblokowac dostep do klas
+        if (name == null) {             // name do produkt1; gdyby nazwa nie zostala ustanowiona w obiekcie
+            if (other.name != null) {   // nazwa w drugim obiekcie nie jest nullem
+                return false;
+            }
+        } else if (!name.equals(other.name)) { // jesli nazwy są nie? rowne, zwroc false;
+            return false;
+        }
+        if(price != other.price)            // ceny sa takie same;
+            return false;
+        return true;
+
+
+
+        return super.equals(obj);
+    }
 }
